9dbec5f074bc851f489d5780d2fb3ec401922bdd
AnnieBRGit <abrantlin@gmail.com>
2021-07-27 13:56:21 +0000

    api test 2 sharp conducted, responses and call ok script.js

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ searchButton.addEventListener('click', (e) => {
     let xhr = new XMLHttpRequest();
     
    //api test ok, responses ok
-
-    xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${input.value}/`);
+    // api test sharp 2 ok, responses ok uppercase lowercase
+    xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=191d5fa7b2969d0320a7c9e00936f23c&units=metric`);
     xhr.onreadystatechange = function() {
         if(this.readyState === XMLHttpRequest.DONE && this.status == 200) {
             let response = JSON.parse(this.response);
             infoCard.innerHTML = `
-            <div> 
-            <img src="${response.sprites.other.dream_world.front_default}">
-            <h1>${response.forms[0].name}</h1>
-            <h2>${response.weight}</h2>           
+            <div>
+            <img src ="">
+            <h1> City: ${response.name}</h1>  
+            <h1> Temp: ${response.main.temp} °C </h1>          
+            <p style="text-transform: uppercase">${response.weather[0].description}</p>
             </div>
             `
         }
