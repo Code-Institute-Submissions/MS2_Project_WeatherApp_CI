ea9dc0e10f0753ab3ea41a7ee0fded1460e42f4e
AnnieBRGit <abrantlin@gmail.com>
2021-07-28 06:42:55 +0000

    index and css files completed

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ searchButton.addEventListener('click', (e) => {
     let input = document.getElementById('userInput');
     let xhr = new XMLHttpRequest();
     
-   //api test ok, responses ok
-    // api test sharp 2 ok, responses ok uppercase lowercase
     xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=191d5fa7b2969d0320a7c9e00936f23c&units=metric`);
     xhr.onreadystatechange = function() {
         if(this.readyState === XMLHttpRequest.DONE && this.status == 200) {
@@ -14,12 +12,17 @@ searchButton.addEventListener('click', (e) => {
             infoCard.innerHTML = `
             <div>
             <img src ="">
-            <h1> City: ${response.name}</h1>  
-            <h1> Temp: ${response.main.temp} °C </h1>          
+            <h2> City: ${response.name}</h2>  
+            <h3> Temp: ${response.main.temp} °C </h3>          
             <p style="text-transform: uppercase">${response.weather[0].description}</p>
             </div>
             `
-        }
+        } //else {
+            //console.log('error');
+           // alert('Please type in correct city name'); // if not city name entered, error alert is generated
+        //}
+        document.getElementById('userInput').value = ""; // this line clears the input box after submit
     }
     xhr.send();
+    
 })
